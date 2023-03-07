d177d652343103028d1891c4db43919f518cdb8e
Dean Tyulev <deantyulev@gmail.com>
2023-03-07 23:13:41 +0200

    Changes to be committed: 	new file:   .vscode/extensions.json 	new file:   README.md 	new file:   index.html 	new file:   package-lock.json 	new file:   package.json 	new file:   public/vite.svg 	new file:   src/App.vue 	new file:   src/main.css 	new file:   src/main.js 	new file:   src/style.css 	new file:   vite.config.js
     Untracked files:
    	.gitignore

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<script setup>
+import { ref, onMounted, computed, watch } from 'vue'
+
+const todos=ref([])
+const name=ref('')
+
+const input_content=ref('')
+const input_category=ref(null)
+
+const todos_asc= computed(() => todos.value.sort((a,b)=>{
+  return a.createdAt -b.createdAt 
+} ))
+
+watch(name,(newval) =>{
+  localStorage.setItem('name',newval)
+})
+onMounted(()=>{name.value=localStorage.getItem('name')||''})
+
+</script>
+
+<template>
+ <main class="app">
+
+  <section class="greeting">
+   <h2 class="title">
+    whats up,<input type="text" placeholder="name here"
+    v-model="name" />
+   </h2> 
+  </section>
+  <section class="create-todo">
+    <h3>Create a Todo</h3>
+ <form @submit.prevent="addtTodo">
+<h4>Whats on your Todo list?</h4>
+<input 
+type="text" 
+placeholder="e.g make a example"
+v-model= "input_content" />
+
+<h4>pick a category</h4>
+<div class="options">
+  <label>
+  <input 
+  type="radio" 
+  name="category" 
+  id="category1"
+  value="bussiness"
+  v-model="input_category">
+  <span class="bubble bussiness"></span>
+  <div>Business</div>
+</label>
+<label>
+  <input 
+  type="radio" 
+  name="category" 
+  id="category1"
+  value="bussiness"
+  v-model="input_category">
+  <span class="bubble bussiness"></span>
+  <div>Business</div>
+</label>
+</div>
+</form>
+
+  </section>
+ </main>
+
+
+</template>
+
